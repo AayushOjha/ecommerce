1d1f42f7621eefe17f88de89b0f13f901e9c554a
Aayushojha <18egjcs024@gitjaipur.com>
2022-03-21 23:39:09 +0530

    app ini..

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>
+        <p>Product List</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
